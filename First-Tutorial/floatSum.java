e82476960aed5e8be0337bf3a4abf36091f78165
Gourav Chaudhary <gouravsittam@gmail.com>
2024-11-20 20:08:38 +0530

    Float Sum

---
+++ First-Tutorial/floatSum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class floatSum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the first value: ");
+        float num1=input.nextInt();
+        System.out.print("Enter the second value: ");
+        float num2=input.nextInt();
+        float sum=num1+num2;
+        System.out.println("the sum is: "+sum);
+
+
+     }
+    
+}
