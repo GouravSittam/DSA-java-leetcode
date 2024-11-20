f95612352bfd6e6c439f1989229d0dddcd444c62
Gourav Chaudhary <gouravsittam@gmail.com>
2024-11-20 21:04:01 +0530

    Conditions in java

---
+++ First-Tutorial/condition.java
@@ -0,0 +1,11 @@
+public class condition {
+    // if condition
+    public static void main(String[] args) {
+        int a = 10;
+        if (a == 10) {
+            System.out.println("Hello World!");
+        } else {
+            System.out.println("Bye World!");
+        }
+    }
+}
