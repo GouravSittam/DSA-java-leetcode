8eeeb2310499583d4fcdad4c652371efbc7c1a6c
Gourav Chaudhary <gouravsittam@gmail.com>
2024-11-20 21:14:52 +0530

    for loop

---
+++ forloop.java
@@ -0,0 +1,13 @@
+public class forloop {
+    public static void main(String[] args){
+
+        for(int count = 1; count !=10; count++)
+        {
+            System.out.println("count is :" +count);
+            
+
+        }
+
+    }
+    
+}
