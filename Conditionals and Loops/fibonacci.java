ce78d3c7fcbbfdf6cb48afc754da9e3c1fdf0765
Gourav Chaudhary <gouravsittam@gmail.com>
2024-11-22 00:53:11 +0530

    fibonacci

--- Conditionals and Loops/fibonacci.java
+++ Conditionals and Loops/fibonacci.java
@@ -4,22 +4,20 @@ public class fibonacci {
     public static void main(String[] args) {
         System.out.print("Enter the number: ");
         Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
-        int p=0;
-        int i=1;
-        int count=2;
+        int n = sc.nextInt();
+        int p = 0;
+        int i = 1;
+        int count = 2;
 
-        while(count<=n){
-            int temp=i;
-            i=i+p;
-            p=temp;
+        while (count <= n) {
+            int temp = i;
+            i = i + p;
+            p = temp;
             count++;
-
 
         }
         System.out.println(i);
 
-
     }
-    
+
 }
