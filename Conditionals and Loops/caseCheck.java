bb8f3b73bbc1acf2c0ed9b39220c7e8ab7cada3d
Gourav Chaudhary <gouravsittam@gmail.com>
2024-11-21 21:54:47 +0530

    caseCheck

--- Conditionals and Loops/caseCheck.java
+++ Conditionals and Loops/caseCheck.java
@@ -4,6 +4,11 @@ public class caseCheck {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         char ch=sc.next().trim().charAt(0);
+        if(ch>='a' && ch<='z'){
+            System.out.println("Lowercase");
+        }else{
+            System.out.println("Uppercase");
+        }
         System.out.println(ch);
         // System.out.println(sc.nextInt().trim());
 
