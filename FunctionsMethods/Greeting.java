2a49e2f6461652e2140901adaffd8c904a5ddd78
Gourav Chaudhary <gouravsittam@gmail.com>
2024-11-23 20:33:17 +0530

    call the function using diff methods

---
+++ FunctionsMethods/Greeting.java
@@ -0,0 +1,11 @@
+// package FunctionsMethods;
+
+public class Greeting {
+    public static void main(String[] args) {
+        greeting();
+        
+    }
+    static void greeting(){
+        System.out.println("Hello World!");
+    }
+}
--- FunctionsMethods/sum.java
+++ FunctionsMethods/sum.java
@@ -1,22 +1,35 @@
-
 import java.util.Scanner;
 
 public class sum {
     public static void main(String[] args) {
-        sum();
-        sum();
-        
+        // sum();
+        // sum();
+        int ans = sum2();
+        System.out.println(ans);
     }
-    static void sum(){
+
+    // return the sum of two numbers
+    static int sum2() {
         System.out.println("Enter the Number you want to add: ");
-        Scanner sc = new Scanner (System.in);
-        int num1,num2,sum;
+        Scanner sc = new Scanner(System.in);
+        // int num1,num2,sum;
         System.out.print("Enter the num1: ");
-        num1=sc.nextInt();
+        int num1 = sc.nextInt();
         System.out.print("Enter the num2: ");
-        num2=sc.nextInt();
-        sum=num1+num2;
+        int num2 = sc.nextInt();
+        int sum = num1 + num2;
+        return sum;
+    }
+
+    static void sum() {
+        System.out.println("Enter the Number you want to add: ");
+        Scanner sc = new Scanner(System.in);
+        int num1, num2, sum;
+        System.out.print("Enter the num1: ");
+        num1 = sc.nextInt();
+        System.out.print("Enter the num2: ");
+        num2 = sc.nextInt();
+        sum = num1 + num2;
         System.out.println(sum);
     }
-    
 }
