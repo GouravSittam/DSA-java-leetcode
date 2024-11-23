5d0f5a4ce61dcc578724656015666658e241da1a
Gourav Chaudhary <gouravsittam@gmail.com>
2024-11-23 20:22:39 +0530

    call the function

--- FunctionsMethods/sum.java
+++ FunctionsMethods/sum.java
@@ -1,18 +1,22 @@
 
 import java.util.Scanner;
 
-public class twoSum {
+public class sum {
     public static void main(String[] args) {
+        sum();
+        sum();
+        
+    }
+    static void sum(){
         System.out.println("Enter the Number you want to add: ");
         Scanner sc = new Scanner (System.in);
         int num1,num2,sum;
-        System.out.println("Enter the num1: ");
+        System.out.print("Enter the num1: ");
         num1=sc.nextInt();
-        System.out.println("Enter the num2: ");
+        System.out.print("Enter the num2: ");
         num2=sc.nextInt();
         sum=num1+num2;
         System.out.println(sum);
-        
     }
     
 }
