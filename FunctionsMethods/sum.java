6fbcae90c35ba01d63b1bb51de52feb1d889ef62
Gourav Chaudhary <gouravsittam@gmail.com>
2024-11-23 20:45:25 +0530

    call the function using diff methods

--- FunctionsMethods/sum.java
+++ FunctionsMethods/sum.java
@@ -6,6 +6,26 @@ public class sum {
         // sum();
         int ans = sum2();
         System.out.println(ans);
+
+        int wer=sum3(30,40);
+        System.out.println(wer);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.next();
+        String personalized = myGreet(name);
+        System.out.println(personalized);
+    }
+
+    static String myGreet(String name) {
+        String msg = "Hello " + name;
+        return msg;
+    }
+
+    //pass the value of numbers when you are calling the methods in main()
+    static int sum3(int a,int b){
+        int sum=a+b;
+        return sum;
     }
 
     // return the sum of two numbers
