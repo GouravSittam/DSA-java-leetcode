7b797669bfe41a0f122c212bf20b1a860ed86d2a
Gourav Chaudhary <gouravsittam@gmail.com>
2024-11-23 20:11:26 +0530

    methods

---
+++ FunctionsMethods/twoSum.java
@@ -0,0 +1,19 @@
+// package FunctionsMethods;
+
+import java.util.Scanner;
+
+public class twoSum {
+    public static void main(String[] args) {
+        System.out.println("Enter the Number you want to add: ");
+        Scanner sc = new Scanner (System.in);
+        int num1,num2,sum;
+        System.out.println("Enter the num1: ");
+        num1=sc.nextInt();
+        System.out.println("Enter the num2: ");
+        num2=sc.nextInt();
+        sum=num1+num2;
+        System.out.println(sum);
+        
+    }
+    
+}
