eb231b1972fffe7cbef8ab8dd16c77e958a22be8
Gourav Chaudhary <gouravsittam@gmail.com>
2024-11-23 21:42:28 +0530

    scope

---
+++ FunctionsMethods/scopeing.java
@@ -0,0 +1,13 @@
+package FunctionsMethods;
+
+public class scopeing {
+    public static void main(String[] args) {
+        int a= 10;
+        int b=20;
+
+    }
+    static void random(){
+        
+    }
+    
+}
