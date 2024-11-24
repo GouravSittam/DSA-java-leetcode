2421d6b0894229c4a66833ac8240aad432324231
Gourav Chaudhary <gouravsittam@gmail.com>
2024-11-24 21:17:32 +0530

    Method overloading

---
+++ FunctionsMethods/FunctionOverloading.java
@@ -0,0 +1,12 @@
+public class FunctionOverloading {
+    public static void main(String[] args) {
+        fun(67);
+        fun("Gourav Chaidhary");
+        
+    }
+    static void fun (int a){
+        System.out.println(a);
+    }
+    static void fun(String name){System.out.println(name);}
+
+}
