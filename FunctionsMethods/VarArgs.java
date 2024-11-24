57b57f1f3a7b412264913f133840dc8a1473b68e
Gourav Chaudhary <gouravsittam@gmail.com>
2024-11-24 21:08:42 +0530

    VarArgs

--- FunctionsMethods/VarArgs.java
+++ FunctionsMethods/VarArgs.java
@@ -3,8 +3,14 @@ import java.util.Arrays;
 public class VarArgs {
     public static void main(String[] args) {
         fun(2,3,45,6,4,5,45,4,4,4,54,5,5,5,5,5,5,3,3,4);
+        mult(1,3,"Hello","World");
 
         
+    }
+
+    static void mult(int a, int b, String ...v){
+        
+
     }
     static void fun(int ...v){
         System.out.println(Arrays.toString(v));
