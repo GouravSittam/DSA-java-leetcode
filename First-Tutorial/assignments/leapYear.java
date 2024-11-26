9d257991bd0e0e7bf7b633522a6cd8fa5b43b141
Gourav Chaudhary <gouravsittam@gmail.com>
2024-11-26 20:06:01 +0530

    leap Year programe

--- First-Tutorial/assignments/leapYear.java
+++ First-Tutorial/assignments/leapYear.java
@@ -6,6 +6,8 @@ public class leapYear{
         System.out.print("Enter the year: ");
         int year = sc.nextInt();
 
+        // int year = 2024;  // specify the year here
+
         if(year%4==0 && year%100!=0 || year%400==0){
             System.out.println(year + " ia a leap year");
         }else{
