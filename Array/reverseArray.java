fe83b4bfed9009ff83a95771759985039e768086
Gourav Chaudhary <gouravsittam@gmail.com>
2024-12-10 17:45:04 +0530

    array

--- Array/reverseArray.java
+++ Array/reverseArray.java
@@ -6,8 +6,6 @@ public class reverseArray {
         reverse(arr);
         System.out.println(Arrays.toString(arr));
     }
-
-
     static void reverse(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
