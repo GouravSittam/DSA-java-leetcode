dcba95513e3df6814c70c16a9075d0b511f3e5b5
Gourav Chaudhary <gouravsittam@gmail.com>
2025-02-05 01:37:00 +0530

    Add Performance class to demonstrate string concatenation efficiency

---
+++ Strings and StringBuilder/Performance.java
@@ -0,0 +1,12 @@
+public class Performance {
+    public static void main(String[] args) {
+        String s = "";
+        for (int i = 0; i < 26; i++) {
+            char ch = (char) ('a' + i);
+            // System.out.println(ch);
+            s+=ch;//combine trhe text to the string 
+        }
+        System.out.println(s);
+
+    }
+}
