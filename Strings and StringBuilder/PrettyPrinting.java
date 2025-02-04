b15cf31d5c00e4b373757b40d39bdc49f11cba14
Gourav Chaudhary <gouravsittam@gmail.com>
2025-02-04 23:40:57 +0530

    Add PrettyPrinting class for formatted output demonstration

---
+++ Strings and StringBuilder/PrettyPrinting.java
@@ -0,0 +1,10 @@
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a=8968.9864f;
+        // System.out.printf("%.2f",a);
+
+        System.out.printf("hello my name is %s and my age is %d","gobu",21);
+    }
+    
+}
