c55b19bc50558d6d70cc8c8b483f466872b09cd6
Gourav Chaudhary <gouravsittam@gmail.com>
2025-02-08 23:28:49 +0530

    Add Sieve of Eratosthenes and prime number checking implementations

---
+++ Maths for Data Structures & Algorithms/SieveofEratosthenes.java
@@ -0,0 +1,27 @@
+public class SieveofEratosthenes {
+    public static void main(String[] args) {
+        int n = 40; // You can change this value to find primes up to any number
+        boolean[] primes = sieveOfEratosthenes(n);
+        System.out.println("Prime numbers up to " + n + ":");
+        for (int i = 2; i <= n; i++) {
+            if (primes[i]) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static boolean[] sieveOfEratosthenes(int n) {
+        boolean[] isPrime = new boolean[n + 1];
+        for (int i = 2; i <= n; i++) {
+            isPrime[i] = true;
+        }
+        for (int p = 2; p * p <= n; p++) {
+            if (isPrime[p]) {
+                for (int i = p * p; i <= n; i += p) {
+                    isPrime[i] = false;
+                }
+            }
+        }
+        return isPrime;
+    }
+}
