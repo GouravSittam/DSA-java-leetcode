b0232bc8a38f9b5dcca588a0f45ad3cf94ca455f
Gourav Chaudhary <gouravsittam@gmail.com>
2025-02-09 23:29:33 +0530

    Add Merge Sort implementation and update Sieve of Eratosthenes formatting

--- Maths for Data Structures & Algorithms/SieveofEratosthenes.java
+++ Maths for Data Structures & Algorithms/SieveofEratosthenes.java
@@ -15,7 +15,7 @@ public class SieveofEratosthenes {
         for (int i = 2; i <= n; i++) {
             isPrime[i] = true;
         }
-        for (int p = 2; p * p <= n; p++) {
+        for (int p = 2; p * p <= n; p++) { 
             if (isPrime[p]) {
                 for (int i = p * p; i <= n; i += p) {
                     isPrime[i] = false;
