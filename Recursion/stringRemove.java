b4a964b66d4b64e201748e9e9ee945f1ef0a3e9f
Gourav Chaudhary <gouravsittam@gmail.com>
2025-02-15 21:15:36 +0530

    Add skipApple method to stringRemove for skipping occurrences of "apple"

--- Recursion/stringRemove.java
+++ Recursion/stringRemove.java
@@ -1,7 +1,8 @@
 public class stringRemove {
     public static void main(String[] args) {
-        String input = "ahiuhhaiohiohahaiohiaohiahiahioahiahiahioahiaaahiahiaaaa";
-        skip("", input);
+        // String input = "ahiuhhaiohiohahaiohiaohiahiahioahiahiahioahiaaahiahiaaaa";
+        skip("", "ahiuhhahiahioahiahiahioahiaaahiahiaaaa");
+        System.out.println(skipApple("ahpplehiaaahiahiaaaa"));
     }
 
     static void skip(String p, String up) {
@@ -15,6 +16,18 @@ public class stringRemove {
             skip(p, up.substring(1));
         } else {
             skip(p + ch, up.substring(1));
+        }
+    }
+
+    static String skipApple(String up){
+        if(up.isEmpty()){
+            return "";
+        }
+
+        if (up.startsWith("apple")){
+            return skipApple(up.substring(5));
+        } else {
+            return up.charAt(0) + skipApple(up.substring(1));
         }
     }
 }
